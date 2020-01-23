98273c9a8124f4bde5da51691d16e49221669909
Jhonston Dalcin <dalcinj@pop-os.localdomain>
2020-01-22 21:45:49 -0300

    Updates

---
+++ precache-manifest.0a57d77f494d1f9813cc330856ca0229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b3d18823a0f84c4899603fdb850123",
+    "url": "/complete-react-developer-zero-to-mastery/index.html"
+  },
+  {
+    "revision": "9954a1de9d0a6b236b82",
+    "url": "/complete-react-developer-zero-to-mastery/static/css/main.33968923.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/complete-react-developer-zero-to-mastery/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/complete-react-developer-zero-to-mastery/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9954a1de9d0a6b236b82",
+    "url": "/complete-react-developer-zero-to-mastery/static/js/main.1ddd64d8.chunk.js"
+  },
+  {
+    "revision": "58deabf68190dc1af936",
+    "url": "/complete-react-developer-zero-to-mastery/static/js/runtime-main.c56f9f8d.js"
+  }
+]);
